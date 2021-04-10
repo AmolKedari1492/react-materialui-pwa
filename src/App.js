30a0b4af943ea4941cc0c5c3237d50ee07b66dda
Amol Kedari <kedarisamols@gmail.com>
2021-04-10 09:26:42 +0530

    Master: Project scaffolding with cra, material and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <AppBar color="primary" position="static">
+          Nav
+          <div>
+            <i className="fa fa-edit">Edit</i>
+          </div>
+        </AppBar>
       </header>
     </div>
   );
-}
+};
 
 export default App;
