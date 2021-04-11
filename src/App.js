07b0f709c69716415bcc7f3945dd9fd0895283f4
Amol Kedari <kedarisamols@gmail.com>
2021-04-11 16:41:55 +0530

    master: react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Nav from "./components/Nav/Nav";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
 import "./App.css";
-import AppBar from "@material-ui/core/AppBar";
 
 const App = () => {
   return (
     <div className="App">
-      <header>
-        <AppBar color="primary" position="static">
-          Nav
-          <div>
-            <i className="fa fa-edit">Edit</i>
-          </div>
-        </AppBar>
-      </header>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
